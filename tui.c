520cf475e9dc57ad30affe20b2b70bb505505837
Joshua Downer <joshua.downer@gmail.com>
2018-12-31 05:45:52 -0500

    set scrollback lines to 8192

--- tui.c
+++ tui.c
@@ -49,7 +49,7 @@ main(int argc, char *argv[])
         GRUVBOX,
         PALETTE_SIZE);
 
-    vte_terminal_set_scrollback_lines(VTE_TERMINAL(terminal), 0);
+    vte_terminal_set_scrollback_lines(VTE_TERMINAL(terminal), 8192);
     vte_terminal_set_scroll_on_output(VTE_TERMINAL(terminal), FALSE);
     vte_terminal_set_scroll_on_keystroke(VTE_TERMINAL(terminal), TRUE);
     vte_terminal_set_rewrap_on_resize(VTE_TERMINAL(terminal), TRUE);
